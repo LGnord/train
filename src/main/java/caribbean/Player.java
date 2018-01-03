fe49d2f3507e8b4149afac5f6445f9b73da86551
ddebarbieux <ddebarbieux.norsys.fr>
2018-01-03 17:54:47 +0100

    permiere version avec les maps, pas terrible

--- src/main/java/caribbean/Player.java
+++ src/main/java/caribbean/Player.java
@@ -10,25 +10,21 @@ import java.util.Scanner;
  **/
 class Player {
 
-	static final int[][][] NEIGHBOUR = { { { 1, 1 }, { 0, 1 }, { -1, 0 }, { -1, -1 }, { -1, 0 }, { 0, 1 } }, //
+	static final int[][][] NEIGHBOUR = { { { 1, 0 }, { 0, -1 }, { -1, -1 }, { -1, 0 }, { -1, 1 }, { 0, 1 } }, //
 			{ { 1, 0 }, { 1, -1 }, { 0, -1 }, { -1, 0 }, { 0, 1 }, { 1, 1 } } };
+	static final int X_MAX = 23;
+	static final int Y_MAX = 21;
 
-	static int[][] goal = { { 0, 0 }, { 0, 0 }, { 0, 0 } };
+	// id -> debut,millieu,fin -> x,y(,speed)
+	static final int[][][] myShips = { { { 0, 0 }, { 0, 0, 0 }, { 0, 0 } }, { { 0, 0 }, { 0, 0, 0 }, { 0, 0 } },
+			{ { 0, 0 }, { 0, 0, 0 }, { 0, 0 } } };
+	static final int[][] myDirection = { { 0, 0, 0 } };
 
-	// bateau id -> devant, milieu, arrière -> {x,y}
-	static int[][][] myShip = { { { 0, 0 }, { 0, 0 }, { 0, 0 } }, { { 0, 0 }, { 0, 0 }, { 0, 0 } },
-			{ { 0, 0 }, { 0, 0 }, { 0, 0 } } };
-	static int[][][] enemiesShip = { { { 0, 0 }, { 0, 0 }, { 0, 0 } }, { { 0, 0 }, { 0, 0 }, { 0, 0 } },
-			{ { 0, 0 }, { 0, 0 }, { 0, 0 } } };
-	static int[][] myShipMoved = { { 0, 0 }, { 0, 0 }, { 0, 0 } };
-	static int mySpeed = 0;
-	static int myOrientation = 0;
-	static int myLastFire = -1;
-	static int currentMoveId = 0;
+	// x,y,direction
+	static int[][] lastMoves = { { 0, 0, 0 }, { 0, 0, 0 }, { 0, 0, 0 } };
 
-	static int dangerNum = 0;
-	static int[][] dangers = { { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 }, { 0, 0 },
-			{ 0, 0 }, };
+	static double[][] paths = new double[X_MAX][Y_MAX];
+	static double[][] fires = new double[X_MAX][Y_MAX];
 
 	public static void main(String args[]) {
 		InputStream in = System.in;
@@ -42,11 +38,8 @@ class Player {
 	static String play(InputStream is) {
 		StringBuffer buffer = new StringBuffer();
 		try {
-			currentMoveId++;
-			Scanner in = new Scanner(is);
 
-			dangerNum = 0;
-			long minDist = 10_000;
+			Scanner in = new Scanner(is);
 
 			int myShipCount = in.nextInt();
 			append(buffer, myShipCount);
@@ -54,7 +47,7 @@ class Player {
 			append(buffer, entityCount);
 
 			int myShipId = 0;
-			int enemiesShipId = 0;
+			initPaths(paths);
 			for (int i = 0; i < entityCount; i++) {
 				int entityId = in.nextInt();
 				String entityType = in.next();
@@ -68,119 +61,180 @@ class Player {
 
 				if ("SHIP".equals(entityType)) {
 					if (arg4 == 1) {
-						myOrientation = arg1;
-						mySpeed = arg2;
-						myShip[myShipId][1][0] = x;
-						myShip[myShipId][1][1] = y;
-						myShip[myShipId][0][0] = x + NEIGHBOUR[x % 2][myOrientation][0];
-						myShip[myShipId][0][1] = y + NEIGHBOUR[x % 2][myOrientation][1];
-						myShip[myShipId][2][0] = x - NEIGHBOUR[x % 2][myOrientation][0];
-						myShip[myShipId][2][1] = y - NEIGHBOUR[x % 2][myOrientation][1];
+						// my ship
+						myShips[myShipId][1][0] = x;
+						myShips[myShipId][1][1] = y;
+						myShips[myShipId][1][2] = arg2; // speed
 						myShipId++;
+						update(paths, x, y, 0.1, 1, 40);
+						// XX
 					} else {
-						int enemiesOrientation = arg1;
-						enemiesShip[enemiesShipId][1][0] = x;
-						enemiesShip[enemiesShipId][1][1] = y;
-						enemiesShip[enemiesShipId][0][0] = x + NEIGHBOUR[x % 2][enemiesOrientation][0];
-						enemiesShip[enemiesShipId][0][1] = y + NEIGHBOUR[x % 2][enemiesOrientation][1];
-						enemiesShip[enemiesShipId][2][0] = x - NEIGHBOUR[x % 2][enemiesOrientation][0];
-						enemiesShip[enemiesShipId][2][1] = y - NEIGHBOUR[x % 2][enemiesOrientation][1];
-						enemiesShipId++;
+						update(paths, x, y, 0.1, -10, 40);
+						update(fires, x, y, 0.5, 50, 5);
 					}
 				}
 
 				if ("MINE".equals(entityType)) {
-					dangers[dangerNum][0] = x;
-					dangers[dangerNum][1] = y;
-					dangerNum++;
+					update(paths, x, y, 0.5, -25, 2);
+					// try to delete mine
+					update(fires, x, y, 0.5, 50, 0);
 				}
 
 				if ("CANNONBALL".equals(entityType)) {
-					dangers[dangerNum][0] = x;
-					dangers[dangerNum][1] = y;
-					dangerNum++;
+					update(paths, x, y, 0.5, -50, 2);
 				}
 
 				if ("BARREL".equals(entityType)) {
-					for (int shipId = 0; shipId < myShipCount; shipId++) {
-						int[] xy = new int[] { x, y };
-						long dist = distance(myShip[shipId][1], xy);
-						if (dist < minDist) {
-							goal[shipId] = xy;
-							minDist = dist;
-						}
-					}
+					update(paths, x, y, 0.9, arg1 * 2, 20);
+					// update(fires, x, y, 0.5, arg1, 1);
 				}
 
 			}
-
-			if (minDist == 10_000) {
-				for (int shipId = 0; shipId < myShipCount; shipId++) {
-					goal[shipId] = enemiesShip[shipId][1];
-				}
-			}
+			System.err.println("My Ships :" + Arrays.deepToString(myShips));
 
 			String res = "";
 
 			for (int shipId = 0; shipId < myShipCount; shipId++) {
 				final String shipRes;
 
-				myShipMoved[2] = myShip[shipId][1];
-				myShipMoved[1] = myShip[shipId][0];
-				myShipMoved[0][0] = myShip[shipId][0][0] + NEIGHBOUR[myShip[shipId][0][0] % 2][myOrientation][0];
-				myShipMoved[0][0] = myShip[shipId][0][1] + NEIGHBOUR[myShip[shipId][0][0] % 2][myOrientation][0];
-
-				boolean isDanger = false;
-				for (int i = 0; i < dangerNum; i++) {
-					for (int[] shipPos : myShipMoved) {
-						if (shipPos[0] == dangers[i][0] && shipPos[1] == dangers[i][1]) {
-							isDanger = true;
-						}
-					}
-				}
-
 				System.err.println("Is :" + buffer);
 				buffer.setLength(0);
-				System.err.println("Enemies ships :" + Arrays.deepToString(enemiesShip));
-				System.err.println("My ships :" + Arrays.deepToString(myShip));
-				System.err.println("My speed :" + mySpeed);
-				System.err.println("My orientation :" + myOrientation);
-				System.err.println("My dangers :" + Arrays.deepToString(dangers));
-				System.err.println("Is danger :" + isDanger);
-				System.err.println("myLastFire, currentMoveId :" + myLastFire + ", " + currentMoveId);
+				System.err.println("Map :" + deepToString(paths, myShips, shipId));
 				System.err.println("\n");
 
-				if (!isDanger) {
-					if (myLastFire + 2 < currentMoveId) {
-						if (distance(myShip[shipId][1], enemiesShip[0][1]) <= 4) {
-							myLastFire = currentMoveId;
-							shipRes = "FIRE " + enemiesShip[0][1][0] + " " + enemiesShip[0][1][1];
-						} else {
-							shipRes = ("MOVE " + goal[shipId][0] + " " + goal[shipId][1]);
+				double maxScore = -1_000_000;
+				int movX = X_MAX / 2, movY = Y_MAX / 2, direction = 0;
+				int[][] movedShip = {};
+				for (int orientation = 0; orientation < 6; orientation++) {
+					int candidateShip[][] = { { 0, 0 }, { 0, 0 }, { 0, 0 } };
+					candidateShip[1][0] = myShips[shipId][1][0] + NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][0];
+					candidateShip[1][1] = myShips[shipId][1][1] + NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][1];
+					candidateShip[0][0] = candidateShip[1][0] - NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][0];
+					candidateShip[0][1] = candidateShip[1][1] - NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][1];
+					candidateShip[2][0] = candidateShip[1][0] + NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][0];
+					candidateShip[2][1] = candidateShip[1][1] + NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][1];
+					if (isInMap(candidateShip)) {
+						double candidateScore = score(paths, candidateShip);
+						if (candidateScore >= maxScore) {
+							movX = candidateShip[1][0] + NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][0];
+							movY = candidateShip[1][1] + NEIGHBOUR[myShips[shipId][1][0] % 2][orientation][1];
+							maxScore = candidateScore;
+							movedShip = candidateShip;
+							direction = orientation;
 						}
-					} else {
-						shipRes = ("MOVE " + goal[shipId][0] + " " + goal[shipId][1]);
 					}
+				}
+				if (direction == lastMoves[shipId][2] && myShips[shipId][1][2] != 0) {
+					shipRes = fire(shipId);
 				} else {
-					int newOrientation = (myOrientation + 1) % 6;
-					shipRes = ("MOVE "
-							+ (myShip[shipId][1][0] + 2 * NEIGHBOUR[myShip[shipId][1][0] % 2][newOrientation][0]) + " "
-							+ (myShip[shipId][1][1] + 2 * NEIGHBOUR[myShip[shipId][1][1] % 2][newOrientation][1]));
+					shipRes = move(shipId, movX, movY, direction, movedShip);
 				}
 				res += shipRes + (shipId == myShipCount - 1 ? "" : "\n");
+				System.err.println("Return'" + shipRes + "' with maxScore=" + maxScore);
 			}
-
+			System.err.println("Return'" + res + "'");
 			return res;
 		} catch (Exception e) {
 			System.err.println("** Exception **");
 			System.err.println("Is :" + buffer);
 			buffer.setLength(0);
-			System.err.println(e.getMessage());
-			return "MOVE 10 10";
+			System.err.println(Arrays.toString(e.getStackTrace()));
+			throw e;
+			// return "MOVE 10 10";
 		}
 	}
 
-	private static void append(StringBuffer buffer, Object... values) {
+	private static String fire(int shipId) {
+		int[] firePos = { 0, 0 };
+		double fireMax = 0;
+		for (int i = 0; i < paths.length; i++) {
+			for (int j = 0; j < paths[i].length; j++) {
+				if (distance(new int[] { i, j }, myShips[shipId][1]) < 10) {
+					if (fires[i][j] > fireMax) {
+						fireMax = fires[i][j];
+						firePos[0] = i;
+						firePos[1] = j;
+					}
+				}
+			}
+		}
+		return "FIRE " + firePos[0] + " " + firePos[1];
+	}
+
+	private static String move(int shipId, int movX, int movY, int direction, int[][] movedShip) {
+		final String shipRes;
+		if (isReplay(shipId, movX, movY)) {
+			System.err.println("Replay");
+			movX += Math.round(Math.random() * 3);
+		}
+		shipRes = "MOVE " + movX + " " + movY;
+		lastMoves[shipId][0] = movX;
+		lastMoves[shipId][1] = movY;
+		lastMoves[shipId][2] = direction;
+		for (int[] shipP : movedShip) {
+			paths[shipP[0]][shipP[1]] -= 1_000; // my ship will be here
+			update(fires, shipP[0], shipP[1], 0.99, -50, 1);
+		}
+		return shipRes;
+	}
+
+	private static boolean isReplay(int shipId, int movX, int movY) {
+		return movX == lastMoves[shipId][0] && movY == lastMoves[shipId][1];
+	}
+
+	private static double score(double[][] map, int[][] ship) {
+		double score = 0;
+		for (int[] shipP : ship) {
+			score += map[shipP[0]][shipP[1]];
+		}
+		return score;
+	}
+
+	private static boolean isInMap(int[][] ship) {
+		for (int[] shipP : ship) {
+			if (shipP[0] < 0 || shipP[1] < 0 || shipP[0] >= X_MAX || shipP[1] >= Y_MAX) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	private static String deepToString(double[][] paths, int[][][] myships, int shipId) {
+		StringBuffer b = new StringBuffer();
+		for (int i = 0; i < paths.length; i++) {
+			boolean newLine = false;
+			for (int j = 0; j < paths[i].length; j++) {
+				int d = distance(new int[] { i, j }, myships[shipId][1]);
+				if (d == 0) {
+					b.append("*");
+				}
+				if (d <= 5) {
+					b.append(paths[i][j]);
+					b.append(", ");
+					newLine = true;
+				}
+			}
+			if (newLine) {
+				b.append("\n");
+			}
+		}
+		return b.toString();
+	}
+
+	private static void initPaths(double[][] paths) {
+		int[] center = { X_MAX / 2, Y_MAX / 2 };
+		for (int i = 0; i < paths.length; i++) {
+			for (int j = 0; j < paths[i].length; j++) {
+				int d = distance(new int[] { i, j }, center);
+				paths[i][j] = 1.0 / (1 + d);
+				if (i == 0 || j == 0 || i == X_MAX - 1 || j == Y_MAX - 1) {
+					paths[i][j] -= 100;
+				}
+			}
+		}
+	}
+
+	static void append(StringBuffer buffer, Object... values) {
 		boolean isFirst = true;
 		for (Object value : values) {
 			if (!isFirst) {
@@ -191,6 +245,24 @@ class Player {
 		}
 		buffer.append(" ");
 
+	}
+
+	static void update(double[][] map, int x, int y, double rate, int score, int distMax) {
+		assert rate < 1;
+		double[] scores = new double[distMax + 1];
+		scores[0] = score;
+		for (int i = 1; i < scores.length; i++) {
+			scores[i] = scores[i - 1] * rate;
+		}
+		int[] xy = { x, y };
+		for (int i = 0; i < map.length; i++) {
+			for (int j = 0; j < map[i].length; j++) {
+				int d = distance(new int[] { i, j }, xy);
+				if (d <= distMax) {
+					map[i][j] += scores[d];
+				}
+			}
+		}
 	}
 
 	static int distance(int[] a, int[] b) {
--- src/test/java/caribbean/PlayerTest.java
+++ src/test/java/caribbean/PlayerTest.java
@@ -14,8 +14,65 @@ public class PlayerTest {
 		InputStream is = new ByteArrayInputStream(in.getBytes());
 		String actual = Player.play(is);
 		System.out.println(actual);
-		Assert.assertFalse("MOVE 19 17".equals(actual));
-		Assert.assertTrue(actual.startsWith("MOVE"));
+		Assert.assertFalse(actual, "MOVE 19 17".equals(actual));
+		Assert.assertTrue(actual, actual.startsWith("MOVE"));
+	}
+
+	@Test
+	public void test_no_exception_when_close_border() {
+		String in = "2 16 0 SHIP 8 9 5 0 84 1 2 SHIP 20 19 4 1 86 1 1 SHIP 7 11 0 0 83 0 3 SHIP 14 14 3 0 99 0 4 MINE 4 8 0 0 0 0 30 CANNONBALL 20 15 3 0 0 0 11 BARREL 4 15 19 0 0 0 10 BARREL 4 5 19 0 0 0 13 BARREL 2 18 20 0 0 0 12 BARREL 2 2 20 0 0 0 14 BARREL 6 8 14 0 0 0 18 BARREL 15 6 16 0 0 0 21 BARREL 16 7 17 0 0 0 23 BARREL 8 10 17 0 0 0 25 BARREL 8 12 12 0 0 0 26 BARREL 13 7 16 0 0 0";
+		InputStream is = new ByteArrayInputStream(in.getBytes());
+		String actual = Player.play(is);
+		Assert.assertTrue(actual, actual.contains("\n"));
+	}
+
+	@Test
+	public void test_move_restart_close_border() {
+		String in = "2 8 0 SHIP 0 4 4 0 24 1 2 SHIP 1 18 2 1 4 1 1 SHIP 9 20 4 0 25 0 3 SHIP 7 19 4 1 30 0 56 CANNONBALL 2 19 1 0 0 0 57 CANNONBALL 2 19 3 1 0 0 58 CANNONBALL 1 19 1 3 0 0 59 CANNONBALL 1 19 3 2 0 0";
+		InputStream is = new ByteArrayInputStream(in.getBytes());
+		String actual = Player.play(is);
+		Assert.assertFalse(actual, actual.contains("MOVE 0 4"));
+	}
+
+	@Test
+	public void test_move_to_closest_barrel() {
+		String in = "3 26 0 SHIP 6 7 4 1 100 1 2 SHIP 9 12 4 1 94 1 4 SHIP 19 13 5 1 98 1 1 SHIP 4 14 2 1 94 0 3 SHIP 5 11 1 1 98 0 5 SHIP 17 12 4 0 97 0 7 MINE 10 14 0 0 0 0 6 MINE 10 6 0 0 0 0 9 MINE 15 12 0 0 0 0 38 CANNONBALL 18 11 5 1 0 0 17 BARREL 21 17 15 0 0 0 16 BARREL 21 3 15 0 0 0 19 BARREL 18 15 13 0 0 0 18 BARREL 18 5 13 0 0 0 22 BARREL 2 13 12 0 0 0 21 BARREL 2 7 12 0 0 0 26 BARREL 13 15 18 0 0 0 25 BARREL 13 5 18 0 0 0 28 BARREL 19 19 16 0 0 0 27 BARREL 19 1 16 0 0 0 30 BARREL 10 15 10 0 0 0 29 BARREL 10 5 10 0 0 0 31 BARREL 17 7 19 0 0 0 34 BARREL 14 12 16 0 0 0 33 BARREL 14 8 16 0 0 0 35 BARREL 19 8 10 0 0 0";
+		InputStream is = new ByteArrayInputStream(in.getBytes());
+		String actual = Player.play(is);
+		Assert.assertFalse(actual, actual.contains("MOVE 8 9"));
+	}
+
+	@Test
+	public void test_event_if_all_neightbours_are_negative() {
+		String in = "3 17 0 SHIP 7 10 5 0 86 1 2 SHIP 7 9 2 0 86 1 4 SHIP 12 3 3 1 63 1 1 SHIP 5 14 1 1 94 0 3 SHIP 8 9 2 0 98 0 5 SHIP 14 16 3 1 66 0 9 MINE 11 1 0 0 0 0 31 CANNONBALL 14 3 3 2 0 0 14 BARREL 3 1 15 0 0 0 19 BARREL 11 15 12 0 0 0 18 BARREL 11 5 12 0 0 0 21 BARREL 12 15 16 0 0 0 20 BARREL 12 5 16 0 0 0 22 BARREL 5 10 19 0 0 0 23 BARREL 9 5 14 0 0 0 25 BARREL 21 1 14 0 0 0 27 BARREL 3 3 17 0 0 0";
+		InputStream is = new ByteArrayInputStream(in.getBytes());
+		String actual = Player.play(is);
+		Assert.assertFalse(actual, actual.contains("MOVE 0 0"));
+	}
+
+	@Test
+	public void test_to_center_when_block_close_border() {
+		String in = "3 13 0 SHIP 4 0 2 0 69 1 2 SHIP 9 0 3 0 65 1 4 SHIP 6 0 3 0 15 1 1 SHIP 3 6 1 1 84 0 3 SHIP 0 4 2 0 68 0 5 SHIP 3 9 1 1 15 0 10 MINE 8 5 0 0 0 0 25 CANNONBALL 6 0 3 1 0 0 26 CANNONBALL 6 0 1 2 0 0 27 CANNONBALL 6 0 3 3 0 0 14 BARREL 1 3 11 0 0 0 16 BARREL 6 2 12 0 0 0 22 BARREL 10 1 19 0 0 0 ";
+		InputStream is = new ByteArrayInputStream(in.getBytes());
+		String actual = Player.play(is);
+		Assert.assertFalse(actual, actual.contains("MOVE 0 0"));
+	}
+
+	@Test
+	public void test_take_barrel_when_distance_1() {
+		String in = "1 2 0 SHIP 4 4 4 0 69 1 1 BARREL 4 3 11 0 0 0 16";
+		InputStream is = new ByteArrayInputStream(in.getBytes());
+		String actual = Player.play(is);
+		Assert.assertTrue(actual, actual.contains("MOVE 4 2"));
+	}
+
+	@Test
+	public void test_no_collosion_with_my_own_ships() {
+		String in = "3 15 0 SHIP 7 5 0 0 88 1 2 SHIP 11 5 3 0 85 1 4 SHIP 16 5 0 0 35 1 1 SHIP 3 6 0 1 98 0 3 SHIP 8 17 5 1 100 0 5 SHIP 21 12 2 1 94 0 6 MINE 16 10 0 0 0 0 7 MINE 21 5 0 0 0 0 9 MINE 11 9 0 0 0 0 11 MINE 13 10 0 0 0 0 12 MINE 15 3 0 0 0 0 31 CANNONBALL 16 5 5 4 0 0 16 BARREL 21 4 17 0 0 0 20 BARREL 9 5 13 0 0 0 22 BARREL 8 2 17 0 0 0 ";
+		InputStream is = new ByteArrayInputStream(in.getBytes());
+		String actual = Player.play(is);
+		// second ship sould move
+		Assert.assertFalse(actual, actual.contains("MOVE 10 5"));
 	}
 
 }
